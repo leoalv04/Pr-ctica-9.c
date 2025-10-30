7dd28512592daf554eb05af7166dfd4c96f325b3
leoalv04 <159968841+leoalv04@users.noreply.github.com>
2025-10-30 14:13:03 -0600

    Create Ejercicio 7.c

---
+++ Ejercicio 7.c
@@ -0,0 +1,15 @@
+#include ‹ stdio.h›
+int maino
+{
+char palabra [20];
+printf("Ingrese una palabra: ");
+scanf("%s", palabra); 
+//* Se omite & porque el propio nombre del arreglo de tipo cadena apunta, es decir, es equivalente a la dirección de comienzo del propio arreglo
+
+printf("La palabra ingresada es: %s\n", palabra);
+for (i = 0; i < 20 ; i++)
+  {
+printf("%c\n", palabra[i]);
+}
+return 0;
+}
